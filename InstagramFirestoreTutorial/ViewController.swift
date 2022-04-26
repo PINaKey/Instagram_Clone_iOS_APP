cfddfae96ff816f95fb5bfc75103d69bbfc2a44a
Pinaki <bandyopadhyay.raj972@gmail.com>
2022-04-25 22:04:25 -0700

    Initial Commit

---
+++ InstagramFirestoreTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by Pinaki Bandyopadhyay on 05/02/1401 AP.
+//  Copyright © 1401 AP Pinaki Bandyopadhyay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
